7f74ae17fdf1b926062e75f584c9b1007d21cea9
BEE-Morenita <info@bioanlov.com>
2026-08-18 18:14:30 -0400

    POLICE

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import "@fontsource-variable/geist";
+import "@fontsource-variable/geist-mono";
 import "./styles/globals.css";
 
 createRoot(document.getElementById("root")!).render(
